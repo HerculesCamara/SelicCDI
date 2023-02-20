d78879388425e806875a812ae74c8fef02071cdd
Hercules Camara <herculescamara2013@gmail.com>
2023-02-20 17:04:11 -0300

    feat: footer creating

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function Footer(){
+    return (
+        <Container>
+            <p>Feito por: <a href="https://www.herculescamara.dev" target={"_blank"}>Hércules C.</a></p>
+        </Container>
+    )
+}
