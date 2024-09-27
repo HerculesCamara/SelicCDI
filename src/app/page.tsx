bc1ee6d370ea9098e868f7809fb7fb5bd111096c
GuilhermeFT <guilhermetrind12@gmail.com>
2024-09-27 16:50:19 -0300

    fix: change colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Header />
 
       <main>
-        <section className="flex h-[calc(100dvh-80px)] bg-gradient-to-b from-sky-700 to-sky-950">
+        <section className="flex h-[calc(100dvh-80px)] bg-gradient-to-b from-zinc-700 to-zinc-950">
           <div className="container my-auto flex h-full max-h-96 max-w-screen-xl flex-col justify-between md:h-max md:flex-row md:items-center">
             <Selic />
           </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import { Logo } from '../logo'
 
 export const Header = () => {
   return (
-    <header className="bg-sky-700">
+    <header className="bg-zinc-700">
       <div className="mx-auto max-w-screen-2xl px-4 py-6 sm:px-6 lg:px-0">
         <Logo />
       </div>
